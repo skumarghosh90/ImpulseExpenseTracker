63ac6eb6affbb6c2f1406f5b0af0f305f169023b
skumarghosh90 <gshark111@gmail.com>
2020-06-21 01:14:31 +0530

    init commit spring boot shell

---
+++ src/main/java/com/impulsetech/ImpulseExpenseTracker/ImpulseExpenseTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.impulsetech.ImpulseExpenseTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpulseExpenseTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpulseExpenseTrackerApplication.class, args);
+	}
+
+}
